bf8fe1f0dafd7321cd57e8b0ffd3502c5abeb224
alexrnov <a.novopashin5@gmail.com>
2020-09-07 09:57:32 +0900

    create settings

--- app/src/main/java/alexrnov/butterflies/details/DetailsActivity.kt
+++ app/src/main/java/alexrnov/butterflies/details/DetailsActivity.kt
@@ -54,17 +54,17 @@ class DetailsActivity : AppCompatActivity() {
     setSupportActionBar(toolbar)
 
     val actionBar = supportActionBar
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true) // enable the Up button
+    actionBar?.let {
+      it.setDisplayHomeAsUpEnabled(true) // enable the Up button
 
       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
         // FROM_HTML_MODE_LEGACY is the behaviour that was used for versions
         // below android N we are using this flag to give a consistent behaviour
-        actionBar.title = Html.fromHtml("<font color='#ffffff'>" +
+        it.title = Html.fromHtml("<font color='#ffffff'>" +
                 this.getString(R.string.app_name) + "</font>", Html.FROM_HTML_MODE_LEGACY)
       } else {
         @Suppress("DEPRECATION")
-        actionBar.title = Html.fromHtml("<font color='#ffffff'>" +
+        it.title = Html.fromHtml("<font color='#ffffff'>" +
                 this.getString(R.string.app_name) + "</font>")
       }
     }
