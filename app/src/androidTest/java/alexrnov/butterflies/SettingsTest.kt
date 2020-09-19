9caf131cbe9a8143cc8a6ad08efcb9a97b2eee7f
alexrnov <a.novopashin5@gmail.com>
2020-09-20 08:35:28 +0900

    add tests

---
+++ app/src/androidTest/java/alexrnov/butterflies/SettingsTest.kt
@@ -0,0 +1,55 @@
+package alexrnov.butterflies
+
+import android.content.pm.ActivityInfo
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest // Marks a test that should run as part of the large tests.
+class SettingsTest {
+
+  @get:Rule
+  var activityRule: ActivityTestRule<MainActivity>
+          = ActivityTestRule(MainActivity::class.java)
+
+  @Test
+  fun changeFontSize() {
+    activityRule.activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+    onView(Matchers.allOf(isDisplayed(), withId(R.id.items_recycler_view)))
+            .perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+    onView(withId(R.id.big_image)).check(ViewAssertions.matches(isDisplayed()))
+    onView(withId(R.id.description_text)).check(ViewAssertions.matches(isDisplayed()))
+    onView(withId(R.id.detailsAppToolbar)).check(ViewAssertions.matches(isDisplayed()))
+
+    onView(withId(R.id.action_settings)).perform(click())
+    onView(withText(R.string.font_size)).perform(click())
+    onView(withText(R.string.very_min_size)).perform(click())
+    // check the appearance of the snackbar
+    onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(ViewAssertions.matches(withEffectiveVisibility(Visibility.VISIBLE)))
+
+    // press up home button
+    onView(withContentDescription(R.string.abc_action_bar_up_description)).perform(click())
+
+    onView(withId(R.id.big_image)).check(ViewAssertions.matches(isDisplayed()))
+    onView(withId(R.id.description_text)).check(ViewAssertions.matches(isDisplayed()))
+    onView(withId(R.id.detailsAppToolbar)).check(ViewAssertions.matches(isDisplayed()))
+
+  }
+}
